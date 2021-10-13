7edaa11aa77d2613dc003b791870f28df90e7e67
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-10-13 19:54:45 -0300

    adicionado layout e lista estática

---
+++ app/src/main/java/br/com/douglas/financaskt/ListaTransacoesActivity.kt
@@ -0,0 +1,22 @@
+package br.com.douglas.financaskt
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$25,00", "Economia - R$100,00")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.adapter = arrayAdapter
+
+    }
+}
